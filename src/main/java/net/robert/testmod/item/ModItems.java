faff6d08bd23b51668bf3379ca70a17d14ea5f8f
RobertsTr <troksaroberts@gmail.com>
2025-06-12 22:50:30 +0300

    item

---
+++ src/main/java/net/robert/testmod/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.robert.testmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.robert.testmod.TestMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
+
+    public static final RegistryObject<Item> BITCOIN = ITEMS.register("bitcoin",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
